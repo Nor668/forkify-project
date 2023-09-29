0c7cc2c046600deaf6760b75df387daae3af7030
Danila <voron102rus@gmail.com>
2023-09-29 20:06:41 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '133c201e-0f6c-4cb8-8a9d-6c77d583e8dd';
+export const MODAL_CLOSE_SEC = 2;
